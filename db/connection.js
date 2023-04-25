3a637806f5cee8faaed8636a1265d962fbff2bee
J Moisés <mcmoy6@gmail.com>
2023-04-25 12:29:46 -0500

    conection bd variable entorno

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,15 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 
-// const dbConnection = new Sequelize('node', 'root', 'Entermoy6', {
+// const dbConnection = new Sequelize(process.env.BD, 'root', process.env.PASSBD, {
 //     host: 'localhost',
 //     port:3306,
 //     dialect: 'mysql',
 //     logging: false
 // });
 
-const dbConnection = new Sequelize('bwobk3prlqiftqyareyd', 'ulz82r5jeg1l90ap', 'A6KnaaRYtclsgz0pCZi4', {
+const dbConnection = new Sequelize(process.env.BD_NAME, process.env.BD_USERNAME, process.env.BD_PASS, {
     host: 'bwobk3prlqiftqyareyd-mysql.services.clever-cloud.com',
     port: 3306,
     dialect: 'mysql',
